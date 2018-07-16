494e4a4462cbf40d70907393719521bbbf3e057a
Andres <ballerlikemike23@gmail.com>
2018-07-15 23:10:00 -0400

    Building the Search View

---
+++ src/js/views/base.js
@@ -0,0 +1,5 @@
+export const elements = {
+  searchForm: document.querySelector('.search'),
+  searchInput: document.querySelector('.seach__field'),
+  searchResList: document.querySelector('.results__list')
+}
